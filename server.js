d4d601db61556d86b8b38be152015cba59d89b41
Alexander Morton <mortonprod@gmail.com>
2017-07-28 03:53:39 +0100

    Add documentation and helmet.
    Also, Remove get routes for static directory

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const fs = require('fs');
 const MongoClient = require('mongodb').MongoClient;
 const stringToObject = require('mongodb').ObjectID
 const mongoStoreFactory = require("connect-mongo");
-const compression = require('compression')
+const compression = require('compression');
+const helmet = require('helmet');
+const url = require('url');
 
 
 var app = express();
+
+app.use(helmet());  
 //Compress here since we do not want to change the build tools. 
 //This will use a bit more CPU as it needs to compress each request and response.
 app.use(compression())
@@ -16,10 +20,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("port", process.env.PORT || 3001);
 console.log("Port: " + process.env.PORT + " mode:  " + process.env.NODE_ENV);
-app.use(express.static("client/build"));
+
+
+/**
+    __ specifies to service worker to overlook cache and always look to server.
+    Map __documentation to static files. Indes will be served first and every other file will be served with the
+    url in src/href prepended with __documentation/frontend ... 
+*/
+app.use("/__documentation/frontend", express.static("client/documentation"));
+/**
+    Don't need to serve index.html about.html since specifying static files will be enough
+*/
+app.use("/", express.static("client/build"));
+
+
 
 var accountsCollection = null; 
-//Hostname(db) comes from service name provide in docker.compose.
+/**
+    We don't need to specify index since this will be served automatically with static files. 
+*/
 MongoClient.connect("mongodb://db:27017", function(err, db) {
   if(!err) {
     console.log("We are connected");
@@ -30,22 +49,6 @@ MongoClient.connect("mongodb://db:27017", function(err, db) {
 
         }
     });
-	app.get('/', function (req, res) {
-	    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-	});
-	app.get('/about', function (req, res) {
-	    res.sendFile(path.join(__dirname+'/client/build/about.html'));
-	});
-	app.get('/services', function (req, res) {
-	    res.sendFile(path.join(__dirname+'/client/build/services.html'));
-	});
-	app.get('/work', function (req, res) {
-	    res.sendFile(path.join(__dirname+'/client/build/work.html'));
-	});
-	app.get('/skills', function (req, res) {
-	    res.sendFile(path.join(__dirname+'/client/build/skills.html'));
-	});
-
 	app.listen(app.get("port"), () => {});
 
   }
