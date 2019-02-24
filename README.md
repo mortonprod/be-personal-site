e6d63c98fa7c239983ef2b040aa98fce6178feca
Alexander Morton <mortonprod@gmail.com>
2019-02-24 22:42:26 +0000

    Feature: SES contact form

--- README.md
+++ README.md
@@ -10,5 +10,19 @@ terraform plan -var-file="variables.tfvar" --auto-approve
 terraform apply -var-file="variables.tfvar" --auto-approve
 
 # Issue
-
+Must verify email first.
 Fix domain name acm setting issue: Always select first domain
+
+
+## CORS
+
+This done outside terraform.
+
+Create OPTIONS method
+Add 200 Method Response with Empty Response Model to OPTIONS method
+Add Mock Integration to OPTIONS method
+Add 200 Integration Response to OPTIONS method
+Add Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Origin Method Response Headers to OPTIONS method
+Add Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Origin Integration Response Header Mappings to OPTIONS method
+Add Access-Control-Allow-Origin Method Response Header to POST method
+Add Access-Control-Allow-Origin Integration Response Header Mapping to POST method 
--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ resource "aws_lambda_function" "lambda" {
   filename         = "lambda.zip"
   function_name    = "ses"
   role             = "${aws_iam_role.role.arn}"
-  handler          = "lambda.lambda_handler"
+  handler          = "index.handler"
   runtime          = "nodejs8.10"
   source_code_hash = "${data.archive_file.file.output_base64sha256}"
 }
@@ -187,6 +187,56 @@ resource "aws_route53_record" "example" {
 
 
 
+
+
+resource "aws_api_gateway_method_response" "options_200" {
+    rest_api_id   = "${aws_api_gateway_rest_api.api_gateway_rest_api.id}"
+    resource_id   = "${aws_api_gateway_resource.api_gateway_resource.id}"
+    http_method   = "${aws_api_gateway_method.api_gateway_method.http_method}"
+    status_code   = "200"
+    response_models {
+        "application/json" = "Empty"
+    }
+    response_parameters {
+        "method.response.header.Access-Control-Allow-Headers" = true,
+        "method.response.header.Access-Control-Allow-Methods" = true,
+        "method.response.header.Access-Control-Allow-Origin" = true
+    }
+    # depends_on = ["aws_api_gateway_method.options_method"]
+}
+# resource "aws_api_gateway_integration" "options_integration" {
+#     rest_api_id   = "${aws_api_gateway_rest_api.api_gateway_rest_api.id}"
+#     resource_id   = "${aws_api_gateway_resource.api_gateway_resource.id}"
+#     http_method   = "${aws_api_gateway_method.api_gateway_method.http_method}"
+#     type          = "MOCK"
+#     # depends_on = ["aws_api_gateway_method.options_method"]
+# }
+# resource "aws_api_gateway_integration_response" "options_integration_response" {
+#     rest_api_id   = "${aws_api_gateway_rest_api.api_gateway_rest_api.id}"
+#     resource_id   = "${aws_api_gateway_resource.api_gateway_resource.id}"
+#     http_method   = "${aws_api_gateway_method.api_gateway_method.http_method}"
+#     status_code   = "${aws_api_gateway_method_response.options_200.status_code}"
+#     response_parameters = {
+#         "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+#         "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+#         "method.response.header.Access-Control-Allow-Origin" = "'*'"
+#     }
+#     depends_on = ["aws_api_gateway_method_response.options_200"]
+# }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # resource "aws_api_gateway_integration_response" "api_gateway_integration_response_200" {
 #   rest_api_id = "${aws_api_gateway_rest_api.api_gateway_rest_api.id}"
 #   resource_id = "${aws_api_gateway_resource.api_gateway_resource.id}"
@@ -218,8 +268,8 @@ resource "aws_route53_record" "example" {
 #   resource_id = "${aws_api_gateway_resource.api_gateway_resource.id}"
 #   http_method = "${aws_api_gateway_method.api_gateway_method.http_method}"
 #   status_code = "200"
-#   response_models = { "application/json" = "${var.response_model}" }
-#   # response_parameters = { "method.response.header.Access-Control-Allow-Origin" = "*" }
+#   # response_models = { "application/json" = "${var.response_model}" }
+#   response_parameters = { "method.response.header.Access-Control-Allow-Origin" = "*" }
 # }
 
 
